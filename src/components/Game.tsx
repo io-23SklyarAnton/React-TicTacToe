31fb1b64707197833c957bc52d86902b73c2bba2
io-23SklyarAnton <80kap.i.toshka@gmail.com>
2024-10-02 11:29:03 +0300

    created simple tictactoe game

---
+++ src/components/Game.tsx
@@ -0,0 +1,54 @@
+import './style.css';
+import Board from './Board';
+import {useState} from 'react';
+
+function Game() {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+    const xIsNext = currentMove % 2 === 0;
+    const currentSquares = history[currentMove];
+
+    function handlePlay(nextSquares: Array<string>) {
+        const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+        setHistory(nextHistory);
+        setCurrentMove(nextHistory.length - 1);
+    }
+
+    function jumpTo(nextMove: number) {
+        setCurrentMove(nextMove);
+    }
+
+    const moves = history.map((_, move) => {
+        let description;
+        if (move === 0) {
+            description = 'Go to game start';
+        } else if (move === currentMove) {
+            return (
+                <li key={move}>
+                    <p>{"You are at move #"}{currentMove}</p>
+                </li>
+            );
+        } else {
+            description = 'Go to move #' + move;
+        }
+        return (
+            <li key={move} className={"historyListElement"}>
+                <button onClick={() => jumpTo(move)}>{description}</button>
+            </li>
+        );
+    });
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+            </div>
+            <div className="game-info">
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+}
+
+
+export default Game;
---
+++ src/components/Square.tsx
@@ -0,0 +1,17 @@
+import './style.css';
+
+interface SquareProps {
+    value: string
+    onSquareClick: () => void
+}
+
+function Square({value, onSquareClick}: SquareProps) {
+
+    return (
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
+    );
+}
+
+export default Square;
